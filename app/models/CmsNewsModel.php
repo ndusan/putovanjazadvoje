a5336c6ad243d4f4316e64aa76ed4d4bd5c8cc60
Dusan Novakovic <ndusan@gmail.com>
2011-12-16 23:29:07 +0100

    set news in cms

--- app/models/CmsNewsModel.php
+++ app/models/CmsNewsModel.php
@@ -3,6 +3,8 @@
 class CmsNewsModel extends Model
 {
     private $tableNews= 'news';
+    private $tableLanguage= 'language';
+    private $tableNewsLanguage= 'news_language';
     
     
     public function findAllNews()
@@ -25,11 +27,36 @@ class CmsNewsModel extends Model
         try{
             $query = sprintf("SELECT * FROM %s WHERE `id`=:id", $this->tableNews);
             $stmt = $this->dbh->prepare($query);
-
+            
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
             $stmt->execute();
-
-            return $stmt->fetch();
+            
+            $object = $stmt->fetch();
+            
+            
+            //Get languages translations
+            $language = array();
+            $query = sprintf("SELECT `l`.`iso_code`, `nl`.* FROM %s AS `l` 
+                                INNER JOIN %s AS `nl` ON `l`.`id`=`nl`.`language_id` 
+                                WHERE `nl`.`news_id`=:newsId", $this->tableLanguage, $this->tableNewsLanguage);
+            $stmt = $this->dbh->prepare($query);
+            
+            $stmt->bindParam(':newsId', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            
+            $collection = $stmt->fetchAll();
+            
+            if(!empty($collection)){
+                foreach ($collection as $k=>$v){
+                    $language[$v['iso_code']]['title'] = $v['title'];
+                    $language[$v['iso_code']]['heading'] = $v['heading'];
+                    $language[$v['iso_code']]['text'] = $v['text'];
+                }
+            }
+            
+            $object['lang'] = $language;
+            
+            return $object;
         }catch(Exception $e){
             
             return false;
@@ -42,20 +69,72 @@ class CmsNewsModel extends Model
     {
         
         try{
-            $query = sprintf("UPDATE %s SET `title_sr`=:titleSr, `heading_sr`=:headingSr, `content_sr`=:contentSr, 
-                                            `title_en`=:titleEn, `heading_en`=:headingEn, `content_en`=:contentEn WHERE `id`=:id", $this->tableNews);
+            //Set language translation
+            foreach($params['content'] as $k=>$v){
+                //Get language_id
+                $query = sprintf("SELECT * FROM %s WHERE `iso_code`=:isoCode", $this->tableLanguage);
+                $stmt = $this->dbh->prepare($query);
+                $stmt->bindParam(':isoCode', $k, PDO::PARAM_STR);
+                $stmt->execute();
+                $response = $stmt->fetch();
+                
+                $query = sprintf("UPDATE %s SET `title`=:title, `heading`=:heading, `text`=:text
+                                    WHERE `news_id`=:newsId AND `language_id`=:languageId",
+                                $this->tableNewsLanguage);
+                
+                $stmt = $this->dbh->prepare($query);
+                
+                $stmt->bindParam(':title', $params['title'][$k], PDO::PARAM_STR);
+                $stmt->bindParam(':heading', $params['heading'][$k], PDO::PARAM_STR);
+                $stmt->bindParam(':text', $v, PDO::PARAM_STR);
+                $stmt->bindParam(':newsId', $params['id'], PDO::PARAM_INT);
+                $stmt->bindParam(':languageId', $response['id'], PDO::PARAM_INT);
+                $stmt->execute();
+            }
+            
+            return true;
+        }catch(Exception $e){
+            
+            return false;
+        }
+    }
+    
+    
+    public function createNews($params)
+    {
+        
+        try{
+            $query = sprintf("INSERT INTO %s SET `created`=CURRENT_TIMESTAMP", $this->tableNews);
             $stmt = $this->dbh->prepare($query);
             
-            $stmt->bindParam(':titleSr', $params['title_sr'], PDO::PARAM_STR);
-            $stmt->bindParam(':headingSr', $params['heading_sr'], PDO::PARAM_STR);
-            $stmt->bindParam(':contentSr', $params['content_sr'], PDO::PARAM_STR);
-            $stmt->bindParam(':titleEn', $params['title_en'], PDO::PARAM_STR);
-            $stmt->bindParam(':headingEn', $params['heading_en'], PDO::PARAM_STR);
-            $stmt->bindParam(':contentEn', $params['content_en'], PDO::PARAM_STR);
-            $stmt->bindParam(':id', $params['id'], PDO::PARAM_INT);
             $stmt->execute();
-
-            return true;
+            
+            $newsId = $this->dbh->lastInsertId();
+            
+            //Set language translation
+            foreach($params['title'] as $k=>$v){
+                //Get language_id
+                $query = sprintf("SELECT * FROM %s WHERE `iso_code`=:isoCode", $this->tableLanguage);
+                $stmt = $this->dbh->prepare($query);
+                $stmt->bindParam(':isoCode', $k, PDO::PARAM_STR);
+                $stmt->execute();
+                $response = $stmt->fetch();
+                
+                $query = sprintf("INSERT INTO %s SET `news_id`=:newsId, `language_id`=:languageId, 
+                                                     `title`=:title, `heading`=:heading, `text`=:text",
+                                $this->tableNewsLanguage);
+                
+                $stmt = $this->dbh->prepare($query);
+                
+                $stmt->bindParam(':newsId', $newsId, PDO::PARAM_INT);
+                $stmt->bindParam(':languageId', $response['id'], PDO::PARAM_INT);
+                $stmt->bindParam(':title', $params['title'][$k], PDO::PARAM_STR);
+                $stmt->bindParam(':heading', $params['heading'][$k], PDO::PARAM_STR);
+                $stmt->bindParam(':text', $v, PDO::PARAM_STR);
+                $stmt->execute();
+            }
+            
+            return $newsId;
         }catch(Exception $e){
             
             return false;
@@ -74,32 +153,6 @@ class CmsNewsModel extends Model
             $stmt->execute();
 
             return $stmt->fetch();
-        }catch(Exception $e){
-            
-            return false;
-        }
-    }
-    
-    
-    
-    public function createNews($params)
-    {
-        
-        try{
-            
-            $query = sprintf("INSERT INTO %s SET `title_sr`=:titleSr, `heading_sr`=:headingSr, `content_sr`=:contentSr,
-                                                 `title_en`=:titleEn, `heading_en`=:headingEn, `content_en`=:contentEn", $this->tableNews);
-            $stmt = $this->dbh->prepare($query);
-            
-            $stmt->bindParam(':titleSr', $params['title_sr'], PDO::PARAM_STR);
-            $stmt->bindParam(':headingSr', $params['heading_sr'], PDO::PARAM_STR);
-            $stmt->bindParam(':contentSr', $params['content_sr'], PDO::PARAM_STR);
-            $stmt->bindParam(':titleEn', $params['title_en'], PDO::PARAM_STR);
-            $stmt->bindParam(':headingEn', $params['heading_en'], PDO::PARAM_STR);
-            $stmt->bindParam(':contentEn', $params['content_en'], PDO::PARAM_STR);
-            $stmt->execute();
-            
-            return $this->dbh->lastInsertId();
         }catch(Exception $e){
             
             return false;
@@ -129,10 +182,17 @@ class CmsNewsModel extends Model
     {
         
         try{
-            $query = sprintf("DELETE FROM %s  WHERE `id`=:id", $this->tableNews);
+            $query = sprintf("DELETE FROM %s WHERE `id`=:id", $this->tableNews);
+            $stmt = $this->dbh->prepare($query);
+            
+            $stmt->bindParam(':id', $params['id'], PDO::PARAM_INT);
+            $stmt->execute();
+            
+            
+            $query = sprintf("DELETE FROM %s WHERE `news_id`=:newsId", $this->tableNewsLanguage);
             $stmt = $this->dbh->prepare($query);
 
-            $stmt->bindParam(':id', $params['id'], PDO::PARAM_INT);
+            $stmt->bindParam(':newsId', $params['id'], PDO::PARAM_INT);
             $stmt->execute();
 
             return true;
