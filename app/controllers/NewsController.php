7bc0cb75e29be1577bf601637c111ef33f8b85f2
Dusan Novakovic <ndusan@gmail.com>
2011-12-17 00:03:13 +0100

    set news on frontend

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -9,8 +9,15 @@ class NewsController extends Controller
      */
     public function indexAction($params)
     {
-
+        $newsId = null;
         
+        if(!empty($params['newsId']) && is_numeric($params['newsId'])){
+            $newsId = $params['newsId'];
+            
+            parent::set('currentNews', $this->db->getSelectedNews($params));
+        }
+
+        parent::set('newsCollection', $this->db->getAllNews($params));
     }
     
     
