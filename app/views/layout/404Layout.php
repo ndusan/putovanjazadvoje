f1949a9b8c5027c35dd64d1563b71bf46077081e
Emil Ajduk <emil@ajduk.com>
2012-01-08 22:05:15 +0100

    404 and header

--- app/views/layout/404Layout.php
+++ app/views/layout/404Layout.php
@@ -10,10 +10,82 @@
         <!-- Load all custom css -->
         <?= $html->css('default', CSS_PATH); ?>
     </head>
-    <body>
-            <!-- This is a content that will be included on page inside of this layout -->
-            <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
-                include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
+    <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>" style="<?= (!empty($bgd['background_color']) ? 'background:' . $bgd['background_color'] : ''); ?> <?= (!empty($bgd['image_name']) ? 'url(' . DS . 'public' . DS . 'uploads' . DS . 'background' . DS . $bgd['image_name'] . ')' : ''); ?> scroll no-repeat 50% 0;">
+        <? if (!empty($bgd['link'])): ?>
+            <a href="<?= $bgd['link']; ?>" target="_blank" class="bgL"></a>
+            <a href="<?= $bgd['link']; ?>" target="_blank" class="bgR"></a>
+        <? endif; ?>
+        <div class="wrapper">
+            <div class="header">
+                <ul class="topNav">
+                    <li><a href="#">Pretplati se</a></li>
+                    <li><a href="#">Naruci ranije brojeve</a></li>
+                    <li><a href="#">Pokloni pretplatu</a></li>
+                </ul>
+                <a class="logo" href="/">
+                    <img width=330" height="60" src="<?= IMAGE_PATH . 'logo.png'; ?>" />
+                </a>
+                <ul class="topInfo">
+                    <li class="first">
+                        <h1>BROJ 18</h1>
+                        <a href="#">broj u prodaji</a>
+                    </li>
+                    <li><img src="<?= IMAGE_PATH . 'topImg.png'; ?>" /></li>
+                </ul>
+            </div>
+            <div class="main">
+                <? include_once VIEW_PATH . 'home' . DS . '_mainNavigation.php'; ?>
+                <div class="content">
+                    <div class="fullBox">
+                        <div class="context">
+                            <h1>404 : Page Not Found</h1>
+                            <p>
+                                Sorry, but the page you are trying to find is not here.
+                            </p>
+                            <!-- This is a content that will be included on page inside of this layout -->
+                            <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
+                                include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footerW">
+            <div class="footer">
+                <div class="copy">
+                    Copyright © putovanjazadvoje.rs 2011.
+                </div>
+                <ul class="footerLinks">
+                    <li class="first"><a href="#"><?= $_t['footer.home.link']; ?></a>
+                        <ul>
+                            <li><a href="#"><?= $_t['footer.magazine.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.actual.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.contests.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.press.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.adv.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.download.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.contact.link']; ?></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><?= $_t['footer.actual.link']; ?></a>
+                        <ul>
+                            <li><a href="#"><?= $_t['footer.magazine.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.actual.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.contests.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.press.link']; ?></a></li>
+                        </ul>
+                    </li>
+                    <li class="last"><a href="#"><?= $_t['footer.contact.link']; ?></a>
+                        <ul>
+                            <li>Email:</li>
+                            <li>Tel:</li>
+                            <li>Skype:</li>
+                            <li>Facebook</li>
+                            <li>Twitter</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </body>
-    
 </html>
--- app/views/layout/defaultLayout.php
+++ app/views/layout/defaultLayout.php
@@ -7,7 +7,7 @@
         <meta name="Description" content="" />
         <meta name="Keywords" content="" />
         <meta http-equiv="X-UA-Compatible" content="IE=7" />
-        <link rel="author" href="<?=DS.'public'.DS.'humans.txt';?>" />
+        <link rel="author" href="<?= DS . 'public' . DS . 'humans.txt'; ?>" />
         <!-- Load all assets (js + css) -->
         <?= $html->assetsJs('jquery-1.6.4.min', ASSETS_JS_PATH); ?>
         <?= $html->assetsJs('slides.min.jquery', ASSETS_JS_PATH); ?>
@@ -24,14 +24,28 @@
         <?= $html->css('default', CSS_PATH); ?>
         <link href='http://fonts.googleapis.com/css?family=Francois+One&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     </head>
-    <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>" style="<?=(!empty($bgd['background_color']) ? 'background:'.$bgd['background_color']:'');?> <?=(!empty($bgd['image_name']) ? 'url('.DS.'public'.DS.'uploads'.DS.'background'.DS.$bgd['image_name'].')' :''); ?> scroll no-repeat 50% 0;">
-        <? if(!empty($bgd['link'])):?>
-        <a href="<?=$bgd['link'];?>" target="_blank" class="bgL"></a>
-        <a href="<?=$bgd['link'];?>" target="_blank" class="bgR"></a>
+    <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>" style="<?= (!empty($bgd['background_color']) ? 'background:' . $bgd['background_color'] : ''); ?> <?= (!empty($bgd['image_name']) ? 'url(' . DS . 'public' . DS . 'uploads' . DS . 'background' . DS . $bgd['image_name'] . ')' : ''); ?> scroll no-repeat 50% 0;">
+        <? if (!empty($bgd['link'])): ?>
+            <a href="<?= $bgd['link']; ?>" target="_blank" class="bgL"></a>
+            <a href="<?= $bgd['link']; ?>" target="_blank" class="bgR"></a>
         <? endif; ?>
         <div class="wrapper">
             <div class="header">
-                <h2>MAGAZIN</h2>
+                <ul class="topNav">
+                    <li><a href="#">Pretplati se</a></li>
+                    <li><a href="#">Naruci ranije brojeve</a></li>
+                    <li><a href="#">Pokloni pretplatu</a></li>
+                </ul>
+                <a class="logo" href="/">
+                    <img width=330" height="60" src="<?= IMAGE_PATH . 'logo.png'; ?>" />
+                </a>
+                <ul class="topInfo">
+                    <li class="first">
+                        <h1>BROJ 18</h1>
+                        <a href="#">broj u prodaji</a>
+                    </li>
+                    <li><img src="<?= IMAGE_PATH . 'topImg.png'; ?>" /></li>
+                </ul>
             </div>
             <div class="main">
                 <? include_once VIEW_PATH . 'home' . DS . '_mainNavigation.php'; ?>
@@ -39,45 +53,45 @@
                     <div class="sideContent">
                         <div class="sideBox">
                             <div class="context">
-                                <form id="search_form" name="search_form" action="<?=DS.$params['lang'].DS.'search';?>" method="get">
-                                    <input id="search_field" type="text" name="q" value="<?=$_t['searchbar.search.label'];?>" title="<?=$_t['searchbar.search.label'];?>" />
+                                <form id="search_form" name="search_form" action="<?= DS . $params['lang'] . DS . 'search'; ?>" method="get">
+                                    <input id="search_field" type="text" name="q" value="<?= $_t['searchbar.search.label']; ?>" title="<?= $_t['searchbar.search.label']; ?>" />
                                     <input type="submit" value="find" />
                                 </form>
                             </div>
                         </div>
                         <div class="sideBox">
                             <div class="title">
-                                <?=$_t['sidebar.onlinecontest.title'];?>
+                                <?= $_t['sidebar.onlinecontest.title']; ?>
                             </div>
                             <div class="context custom">
                                 <img src="<?= IMAGE_PATH . 'baner1.jpg'; ?>" />
-                                <? if(!empty($onlineCompetitionCollection)): ?>
-                                <? foreach($onlineCompetitionCollection as $onlineCompetition):?>
-                                <?=$onlineCompetition['text'];?>
-                                <? endforeach;?>
-                                <? endif;?>
+                                <? if (!empty($onlineCompetitionCollection)): ?>
+                                    <? foreach ($onlineCompetitionCollection as $onlineCompetition): ?>
+                                        <?= $onlineCompetition['text']; ?>
+                                    <? endforeach; ?>
+                                <? endif; ?>
                             </div>
                         </div>
                         <div class="sideBox">
                             <div class="title">
-                                <?=$_t['sidebar.contest.title'];?>
+                                <?= $_t['sidebar.contest.title']; ?>
                             </div>
                             <div class="context custom">
                                 <img src="<?= IMAGE_PATH . 'baner1.jpg'; ?>" />
-                                <? if(!empty($offlineCompetitionCollection)): ?>
-                                <? foreach($offlineCompetitionCollection as $offlineCompetition):?>
-                                <?=$offlineCompetition['text'];?>
-                                <? endforeach;?>
-                                <? endif;?>
+                                <? if (!empty($offlineCompetitionCollection)): ?>
+                                    <? foreach ($offlineCompetitionCollection as $offlineCompetition): ?>
+                                        <?= $offlineCompetition['text']; ?>
+                                    <? endforeach; ?>
+                                <? endif; ?>
                             </div>
                         </div>
-                        <? if(!empty($bannerCollection)):?>
-                        <ul class="banners">
-                            <? foreach($bannerCollection as $banner):?>
-                            <li><img src="<?= DS . 'public' . DS . 'uploads' . DS . 'banners' . DS . 'thumb-'.$currentNews['image_name']; ?>" /></li>
-                            <? endforeach;?>
-                        </ul>
-                        <? endif;?>
+                        <? if (!empty($bannerCollection)): ?>
+                            <ul class="banners">
+                                <? foreach ($bannerCollection as $banner): ?>
+                                    <li><img src="<?= DS . 'public' . DS . 'uploads' . DS . 'banners' . DS . 'thumb-' . $currentNews['image_name']; ?>" /></li>
+                                <? endforeach; ?>
+                            </ul>
+                        <? endif; ?>
                     </div>
 
                     <!-- This is a content that will be included on page inside of this layout -->
@@ -96,26 +110,26 @@
                     Copyright © putovanjazadvoje.rs 2011.
                 </div>
                 <ul class="footerLinks">
-                    <li class="first"><a href="#"><?=$_t['footer.home.link'];?></a>
+                    <li class="first"><a href="#"><?= $_t['footer.home.link']; ?></a>
                         <ul>
-                            <li><a href="#"><?=$_t['footer.magazine.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.actual.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.contests.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.press.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.adv.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.download.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.contact.link'];?></a></li>
+                            <li><a href="#"><?= $_t['footer.magazine.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.actual.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.contests.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.press.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.adv.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.download.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.contact.link']; ?></a></li>
                         </ul>
                     </li>
-                    <li><a href="#"><?=$_t['footer.actual.link'];?></a>
+                    <li><a href="#"><?= $_t['footer.actual.link']; ?></a>
                         <ul>
-                            <li><a href="#"><?=$_t['footer.magazine.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.actual.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.contests.link'];?></a></li>
-                            <li><a href="#"><?=$_t['footer.press.link'];?></a></li>
+                            <li><a href="#"><?= $_t['footer.magazine.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.actual.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.contests.link']; ?></a></li>
+                            <li><a href="#"><?= $_t['footer.press.link']; ?></a></li>
                         </ul>
                     </li>
-                    <li class="last"><a href="#"><?=$_t['footer.contact.link'];?></a>
+                    <li class="last"><a href="#"><?= $_t['footer.contact.link']; ?></a>
                         <ul>
                             <li>Email:</li>
                             <li>Tel:</li>
