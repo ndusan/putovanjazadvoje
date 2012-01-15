22011788b5b4789e01713e3859454c2d1a1948a7
Dusan Novakovic <ndusan@gmail.com>
2012-01-15 19:34:36 +0100

    display winners

--- app/models/CmsContestModel.php
+++ app/models/CmsContestModel.php
@@ -539,7 +539,7 @@ class CmsContestModel extends Model
         try{
             if(!empty($params['winner'])){
                 foreach($params['winner'] as $key=>$value){
-                    $query = sprintf("UPDATE %s SET `winner`=:winner WHERE `id`=:id", $this->tableContestPrize);
+                    $query = sprintf("UPDATE %s SET `winner`=:winner WHERE `id`=:id ORDER BY `id` DESC", $this->tableContestPrize);
                     $stmt = $this->dbh->prepare($query);
 
                     $stmt->bindParam(':winner', $value, PDO::PARAM_STR);
