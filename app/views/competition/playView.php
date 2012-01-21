17faf44c050d6a7b31a6aec6e6f7d8091ae86133
Dusan Novakovic <ndusan@gmail.com>
2012-01-21 17:51:45 +0100

    set game

--- app/views/competition/playView.php
+++ app/views/competition/playView.php
@@ -1,10 +1,10 @@
 <style type="text/css">
 	body { font-family:Verdana, Arial, Helvetica, sans-serif; }
 	#container { width:100%; float:left; border-bottom:1px solid #000000; border-top:1px solid #000000; padding: 20px 0; margin: 10px 0; }
-	#puzzle { width: 310px; float:left; }
-	#sortable { list-style-type: none; margin:0px; padding: 0px; width:309px; height:309px; background:url(http://rohitsengar.cueblocks.net/puzzle/images/img1.jpg) center center no-repeat; }
+	#puzzle { float:left; }
+	#sortable { list-style-type: none; margin:0px; padding: 0px;}
 	#sortable li { margin: 3px 3px 0 0; float: left; width: 100px; height: 100px; color:#FFFFFF; }
-	#stats { background-color:#DDFFAA; margin-top:25px; width:309px; text-align: center; font-size:18px; }
+	#stats { background-color:#DDFFAA; margin-top:25px; text-align: center; font-size:18px; }
 	#time, #moves { font-size: 26px; }
 	#start { background-color: #000000; color:#FFFFFF; opacity: 0.75; font-size:16px; text-align:center; padding: 15px 0; margin: 135px 0; width: 100%; float:left; cursor:pointer; }
 	
@@ -20,12 +20,15 @@
 
 <div id="container">
     <!-- Image Puzzle -->
-    <div id="puzzle">
+    <div id="puzzle" style="width: 310px;">
         <!-- Image Puzzle -->
-        <ul id="sortable">
+        <ul id="sortable" style="background:url(http://rohitsengar.cueblocks.net/puzzle/images/img1.jpg) center center no-repeat; width:309px; height:309px;">
                 <div id="start">Click to Start</div>
         </ul>
         <!-- Puzzle Stats -->
-        <div id="stats"><span id="moves">0</span> moves, <span id="time">0</span> seconds</div>
+        <div id="stats" style="width:309px;">
+            <span id="moves">0</span> moves, 
+            <span id="time">0</span> seconds
+        </div>
     </div>
 </div>
