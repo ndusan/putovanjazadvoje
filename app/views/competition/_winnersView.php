c811cce941a56cf18e18af418e7818e68fcbc6e8
Emil Ajduk <emil@ajduk.com>
2012-03-30 21:50:59 +0200

    css

--- app/views/competition/_winnersView.php
+++ app/views/competition/_winnersView.php
@@ -15,23 +15,26 @@
                             <!-- Contest name -->
                             <h2><?= $w['name']; ?></h2>
                             <!-- Contest description -->
-                            <?= $w['content']; ?>
+                            <div class="wys">                           
+                                <?= $w['content']; ?>
+                            </div>
                             <!-- Winner image if set -->
-                            <? if(!empty($w['winner_image_name'])):?>
-                            <img src="<?= PUBLIC_UPLOAD_PATH .'contest'.DS.$w['winner_image_name']; ?>" />
-                            <? endif;?>
-                                    
+                            <? if (!empty($w['winner_image_name'])): ?>
+                                <img src="<?= PUBLIC_UPLOAD_PATH . 'contest' . DS . $w['winner_image_name']; ?>" />
+                            <? endif; ?>
+
                             <? if (!empty($w['winners'])): ?>
                                 <? foreach ($w['winners'] as $winner): ?>
                                     <!-- Winner name -->
+                                    <h2 class="prize"><?= $winner['title']; ?> : <?= $winner['winner']; ?></h2>
                                     <div class="wys">
-                                    <h2><?=$winner['title'];?></h2>
-                                    <?=$winner['content'];?>
-                                    <h3><?=$winner['winner'];?></h3>
+                                        <h3><?= $_t['prize.label']; ?> :</h3>
+                                        <?= $winner['content']; ?>
                                     </div>
-                                    <? if(!empty($winner['image_name'])):?>
-                                    <img src="<?= PUBLIC_UPLOAD_PATH .'contest'.DS.$winner['image_name']; ?>" />
-                                    <? endif;?>
+                                    <? if (!empty($winner['image_name'])): ?>
+                                        <img src="<?= PUBLIC_UPLOAD_PATH . 'contest' . DS . $winner['image_name']; ?>" />
+                                    <? endif; ?>
+
                                 <? endforeach; ?>
                             <? endif; ?>
                         </li>
