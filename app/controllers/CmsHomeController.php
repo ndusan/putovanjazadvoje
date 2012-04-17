101b9de38920d8901d91a1e52b2392bcaf883913
Dusan Novakovic <ndusan@gmail.com>
2012-04-17 10:10:55 +0200

    including GA

--- app/controllers/CmsHomeController.php
+++ app/controllers/CmsHomeController.php
@@ -9,22 +9,41 @@ class CmsHomeController extends Controller
         
         // enter your login, password and id into the variables below to try it out
 
-//        $login = GAQ_USERNAME;
-//        $password = GAQ_PASSWORD;
-//
-//        // NOTE: the id is in the form ga:12345 and not just 12345
-//        // if you do e.g. 12345 then no data will be returned
-//        // read http://www.electrictoolbox.com/get-id-for-google-analytics-api/ for info about how to get this id from the GA web interface
-//        // or load the accounts (see below) and get it from there
-//        // if you don't specify an id here, then you'll get the "Badly formatted request to the Google Analytics API..." error message
-//        $id = GAQ_PROFILE_ID;
-//
-//        $api = new analytics_api();
-//        if($api->login($login, $password)) {
-//
-//                parent::set('visitors', $api->data($id, '', 'ga:bounces,ga:newVisits,ga:visits,ga:pageviews,ga:uniquePageviews'));
-//                parent::set('visitorsToday', $api->get_summary($id, 'today'));
-//        }
+        $login = GAQ_USERNAME;
+        $password = GAQ_PASSWORD;
+
+        // NOTE: the id is in the form ga:12345 and not just 12345
+        // if you do e.g. 12345 then no data will be returned
+        // read http://www.electrictoolbox.com/get-id-for-google-analytics-api/ for info about how to get this id from the GA web interface
+        // or load the accounts (see below) and get it from there
+        // if you don't specify an id here, then you'll get the "Badly formatted request to the Google Analytics API..." error message
+        $id = GAQ_PROFILE_ID;
+        
+        $set = array('visitors'=>false, 'visitorsToday'=>false);
+        
+        $data['visitors'] = Cache::get(array('key'=>'visitors'));
+        if (false != $data['visitors']) {
+            $set['visitors'] = true;
+        }
+        
+        $data['visitorsToday'] = Cache::get(array('key'=>'visitorsToday'));
+        if (false != $data['visitorsToday']) {
+            $set['visitorsToday'] = true;
+        }
+        
+        if (false == $set['visitors'] || false == $set['visitorsToday']) {
+            $api = new analytics_api();
+            if($api->login($login, $password)) {
+                $data = array('visitors' => $api->data($id, '', 'ga:bounces,ga:newVisits,ga:visits,ga:pageviews,ga:uniquePageviews'),
+                              'visitorsToday' => $api->get_summary($id, 'yesterday'));
+        
+                Cache::set(array('key'=>'visitors', 'data'=>$data['visitors']));
+                Cache::set(array('key'=>'visitorsToday', 'data'=>$data['visitorsToday']));
+            }
+        }
+        
+        $this->set('visitors', $data['visitors']);
+        $this->set('visitorsToday', $data['visitorsToday']);
     }
     
     public function settingsAction($params)
